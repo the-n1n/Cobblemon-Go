684358bba001fabb84c0dd733f1b4bc0572f7daa
the-n1n <mosiahwf@gmail.com>
2025-06-18 03:33:25 -0700

    Fix Template

--- src/main/java/cobblemon/n1npogo/CobblemonGo.java
+++ src/main/java/cobblemon/n1npogo/CobblemonGo.java
@@ -1,12 +1,12 @@
-package com.example;
+package cobblemon.n1npogo;
 
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class CobblemonGo implements ModInitializer {
+	public static final String MOD_ID = "cobblemon-go";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
